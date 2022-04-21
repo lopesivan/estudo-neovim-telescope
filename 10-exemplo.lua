320a23c287e9b3b3bdfb997f9fa75aaeb08ceb3f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-04-21 16:30:18 -0300

    10-exemplo.lua

---
+++ 10-exemplo.lua
@@ -0,0 +1,29 @@
+local pickers = require "telescope.pickers"
+local finders = require "telescope.finders"
+local sorters = require "telescope.sorters"
+local dropdown = require "telescope.themes".get_dropdown()
+
+local mini ={
+  layout_strategy = "vertical",
+  layout_config = {
+		width = 100,
+		height = 80,
+		prompt_position = "top",
+  },
+  sorting_strategy = "ascending",
+}
+
+local opts = {
+  finder = finders.new_table {
+	"/home/ivan/.config/nvim/templates/go/models/hello.md",
+	"/home/ivan/.config/nvim/templates/go/models/hello/hello.go",
+	"/home/ivan/.config/nvim/templates/go/models/hello/go.mod",
+  },
+
+  sorters = sorters.get_generic_fuzzy_sorter({}),
+}
+
+local colors = pickers.new(dropdown, opts)
+
+colors:find()
+
