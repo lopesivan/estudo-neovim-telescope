33aee29b4e54aebe751b4266d8a98a9e413f9e5f
Ivan Lopes <lopesivan.ufrj@gmail.com>
2022-04-21 20:13:55 -0300

    fix(sorters -> sorter) linha 8

--- 20-exemplo.lua
+++ 20-exemplo.lua
@@ -2,16 +2,16 @@ local pickers = require "telescope.pickers"
 local finders = require "telescope.finders"
 local sorters = require "telescope.sorters"
 
--- local input = {'rg', '--line-number', '--column', "", vim.fn.expand('~/.config/nvim/w/data/example')}
-local input = {'rg', '', vim.fn.expand('~/.config/nvim/w/data/example')}
+local input = {"rg", "--line-number", "--column", "", vim.fn.expand('~/.config/nvim/w/data/example')}
 
 local opts = {
   finder = finders.new_oneshot_job(input),
-  sorters = sorters.get_generic_fuzzy_sorter(),
+  sorter = sorters.get_generic_fuzzy_sorter(),
 }
 
 local picker = pickers.new(opts)
 
 picker:find()
 
---  FIXME: 💩   > Não funciona conforme o previsto
+--  FIXME: 💩   > funciona conforme o previsto, o bug estava no sorter
+--  fix(sorters -> sorter) linha 8
