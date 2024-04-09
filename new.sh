68fb8e3a42620fed5f7c0c0ab8063da32d245213
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-04-09 12:01:48 -0300

    exemplos harpoon

--- new.sh
+++ new.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-ls *-exemplo.lua |
-xargs -L1        |
-sort -n          |
-tail -1          |
-cut -d'-' -f1    |sed 's/^0//'|
-while read n; do printf "cp %02d-exemplo.lua %02d-exemplo.lua\n" "$n" "$((n + 1))"; done
+suffix=exemplo
+suffix=harpoon
+ls *-${suffix}.lua |
+	xargs -L1 |
+	sort -n |
+	tail -1 |
+	cut -d'-' -f1 | sed 's/^0//' |
+	while read n; do printf "cp %02d-${suffix}.lua %02d-${suffix}.lua\n" "$n" "$((n + 1))"; done
